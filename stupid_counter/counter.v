581ef34eefa612ddc6a2ad96f98c3f1808c2a9d4
Dhilan Teeluckdharry <143971033+inhald@users.noreply.github.com>
2025-02-01 15:57:36 -0500

    Add files via upload

--- stupid_counter/counter.v
+++ stupid_counter/counter.v
@@ -1,15 +1,20 @@
 module counter (
-	input 		clk,
-	input			reset,
-	output reg [3:0]	count
+	input clk,
+	input	reset,
+	output reg [3:0] count
 
 );
 
 	always @(posedge clk or negedge reset) begin
-		if (!reset)
-			count <= 4'b0;		
-		else 
-		count <= count + 4'd1;
+		if (!reset) begin
+			count <= 4'b0;
+		end
+		else if (count == 4'd9) begin
+			count <= 4'b0;
+		end
+		else begin 
+			count <= count + 4'd1;
+		end
 	
 	end
 
